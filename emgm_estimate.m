814ba96f38841637f3f1beea109d937eafd21565
Accacio <raccacio@poli.ufrj.br>
2022-01-10 18:07:07 +0100

    remove plots

--- emgm_estimate.m
+++ emgm_estimate.m
@@ -1,9 +1,6 @@
 function [C,d,responsabilities,pi,Sigma] = emgm_estimate(x,y,modes,emMaxIter,maxErr,m,n,colors)
 % EMGM_ESTIMATE -
 
-    pi=repmat(1/modes,1,modes);
-    OldclusterSize=zeros(1,modes);
-
     % TODO(accacio): find way to initialize it better (use gradient)
     % Something like:
     % indexpts1=randi([1 N-1],1,3);
@@ -19,26 +16,14 @@ function [C,d,responsabilities,pi,Sigma] = emgm_estimate(x,y,modes,emMaxIter,max
     % C=10*rand(size(m));
     % d=10*rand(size(n));
 
+    pi=repmat(1/modes,1,modes);
     eps=10;
     Sigma(:,:,1:modes)=repmat(eps*eye(1),1,1,modes);
 
+    OldclusterSize=zeros(1,modes);
     for emInd=1:emMaxIter
 
         responsabilities=calculate_responsabilities(x,y,C,d,Sigma,pi);
-
-
-        figure(1)
-        subplot(2,1,2)
-        cla
-        plot_responsibles(x, y, responsabilities, C, d, colors);
-        view(7,17)
-        title(['EM Gaussian Mixture iter=' num2str(emInd) ])
-        zlim([min(y) max(y)])
-        xlim([min(x(1,:)) max(x(1,:))])
-        ylim([min(x(2,:)) max(x(2,:))])
-        xlabel('x1')
-        ylabel('x2')
-
 
         [C, d, pi] = update_parameters(x, y, responsabilities);
         [~,z_hat]=max(responsabilities,[],1);
@@ -56,5 +41,4 @@ function [C,d,responsabilities,pi,Sigma] = emgm_estimate(x,y,modes,emMaxIter,max
         end
         OldclusterSize=clusterSize;
     end
-
 end
